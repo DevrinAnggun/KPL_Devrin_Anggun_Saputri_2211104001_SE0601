339db13204a48e116d72cd7544cb11627ec8aae4
Devrin Anggun Saputri <devrinanggunsaputri90@gmail.com>
2025-03-04 22:35:37 +0700

    Menambahkan TP_02

---
+++ 02_Pengenalan_IDE_dan_Pemrograman_C#/TP/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Masukkan satu huruf: ");
+        char huruf = Console.ReadKey().KeyChar;
+        Console.WriteLine();
+
+        if (char.IsLetter(huruf) && char.IsUpper(huruf))
+        {
+            if ("AIUEO".Contains(huruf))
+            {
+                Console.WriteLine($"Huruf {huruf} merupakan huruf vokal");
+            }
+            else
+            {
+                Console.WriteLine($"Huruf {huruf} merupakan huruf konsonan");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Input harus berupa satu huruf kapital!");
+        }
+
+        // Menambahkan array berisi 5 bilangan genap
+        int[] angkaGenap = { 2, 4, 6, 8, 10 };
+
+        // Iterasi untuk mencetak setiap elemen dalam array
+        for (int i = 0; i < angkaGenap.Length; i++)
+        {
+            Console.WriteLine($"Angka genap {i + 1} : {angkaGenap[i]}");
+        }
+    }
+}
---
+++ 02_Pengenalan_IDE_dan_Pemrograman_C#/TP/soal A.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Masukkan satu huruf: ");
+        char huruf = Console.ReadKey().KeyChar;
+        Console.WriteLine();
+
+        if (char.IsLetter(huruf) && char.IsUpper(huruf))
+        {
+            if ("AIUEO".Contains(huruf))
+            {
+                Console.WriteLine($"Huruf {huruf} merupakan huruf vokal");
+            }
+            else
+            {
+                Console.WriteLine($"Huruf {huruf} merupakan huruf konsonan");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Input harus berupa satu huruf kapital!");
+        }
+    }
+}
