a9d8e148f51500ded574b1c58f7d2aedbc5c4cf7
Devrin Anggun Saputri <devrinanggunsaputri90@gmail.com>
2025-03-26 12:30:12 +0700

    menambahkan class GlosaryItem2211104001

---
+++ 07_Grammar-Based_Input_Processing_Parsing/JURNAL/File Code/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace jurnal_modul7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("=== Member List ===\n");
+
+            // Memanggil method ReadJSON() dari class TeamMahasiswa2211104001
+            TeamMembers2211104001.ReadJSON();
+        }
+    }
+}
