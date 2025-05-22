f269b4e65b3e83dc2cc92886afbecfb7704d16b2
Devrin Anggun Saputri <devrinanggunsaputri90@gmail.com>
2025-05-22 14:01:43 +0700

    upload praktikum modul 14

---
+++ 14_Clean_Code/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace CleanCode
+{
+    // Kelas ini bertanggung jawab hanya untuk mengelola pengguna
+    public class User
+    {
+        public string Name { get; private set; }
+        public DateTime DateOfBirth { get; private set; }
+
+        public User(string name, DateTime dateOfBirth)
+        {
+            Name = name;
+            DateOfBirth = dateOfBirth;
+        }
+
+        public int CalculateAgeInYears()
+        {
+            var today = DateTime.Today;
+            var age = today.Year - DateOfBirth.Year;
+
+            if (DateOfBirth.Date > today.AddYears(-age)) age--;
+
+            return age;
+        }
+
+        public void DisplayUserInfo()
+        {
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Age: {CalculateAgeInYears()} years");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var user = new User("Devrin Cantik", new DateTime(2004, 05, 02));
+            user.DisplayUserInfo();
+        }
+    }
+}
